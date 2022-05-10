db1bc4cc690c49a9f39080cf10a79161696910d4
ashuthosh-mr <66374530+ashuthosh-mr@users.noreply.github.com>
2022-05-10 15:09:19 +0530

    Update barrier.c

--- barrier.c
+++ barrier.c
@@ -1,5 +1,5 @@
 // Call barrier(no. of threads); in the multi-threaded C program to implement barrier
-void barrier(int c)
+void barrier(int n)
 {
 asm volatile ("li t0, 1\n" 
               "li x31,0x0000000081000004\n" //hope nothing is in this address
@@ -12,9 +12,9 @@ asm volatile ("li t0, 1\n"
               "amoswap.w.rl x0, x0, (x31)\n"); //release lock
 int check=0;
 spin: asm ("lw t2, 4(x31)" :"=r"(check)); //read the contents of 4(x31)
-if(check==c) // if it is equal to 4 (4 is the number of threads used) 
+if(check==c) // if it is equal to n (n is the number of threads used) 
 return;
 else 
-goto spin; // spin if the check is not equal to 4
+goto spin; // spin if the check is not equal to n
 
 }
